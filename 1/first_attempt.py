33ceecc89ad41e6240ab8dd4d94dfcc9a2738c39
Arash Hajisafi <34424672+Arashhs@users.noreply.github.com>
2021-07-28 06:08:17 +0430

    Implemented function to score bias degree using a cosine similarity in my representation. Implemented function to parse SQL query. Added more datasets

--- 1/first_attempt.py
+++ 1/first_attempt.py
@@ -1,13 +1,18 @@
 import os
+import math
+import re
 import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine, types
+import sqlparse
 
 dataset_dir = 'dataset'
 database_name = 'simpson_database'
 db_user = 'root'
 db_pass = ''
 db_host = 'localhost'
+
+bias_degree_threshold = 0.7
 
 # create SQL tables from CSV dataset
 def init_sql_db(dataset_directory):
@@ -21,6 +26,29 @@ def init_sql_db(dataset_directory):
         df = pd.read_csv(csv_files[i],sep=',',quotechar='\'',encoding='utf8')
         df.to_sql(dataset_name,con=mysql_engine,index=False,if_exists='replace')
     return df
+
+
+# getting normalized of a vector
+def get_normalized(vector):
+    vec_len = sum([v**2 for v in vector])
+    vec_len = math.sqrt(vec_len)
+    if vec_len == 0:
+        return vector
+    norm_vec = [v/vec_len for v in vector]
+    return norm_vec
+
+
+# calculating cosine score between two vectors
+def get_cosine_score(vec1, vec2):
+    v1 = get_normalized(vec1)
+    v2 = get_normalized(vec2)
+    cosine_sim = np.dot(v1, v2)
+    return cosine_sim
+
+
+# get the bias degree
+def get_bias_degree(vec1, vec2):
+    return 1 - get_cosine_score(vec1, vec2)
 
 
 # get the desired groups based on common attributes in dataset
@@ -62,29 +90,119 @@ def is_biased(first_gp_avgs, second_gp_avgs):
                 continue
             current_count += np.sum([second_gp_avgs[agg_att_val, test_att_val] >= second_gp_avgs[val, test_att_val] for val in agg_attribute_vals if (val, test_att_val) in second_gp_avgs]) - 1
         test_bigger_num.append(current_count)
-    if agg_bigger_num.index(max(agg_bigger_num)) > test_bigger_num.index(max(test_bigger_num)) and max(test_bigger_num) != 0:
-        return True
-    return False
-    
-
-    
+    bias_degree = get_bias_degree(agg_bigger_num, test_bigger_num)
+    # if test_bigget_num is all zeroes or the bias degree is lower than the threshold, return False
+    if np.all(np.array(test_bigger_num) == 0) or bias_degree < bias_degree_threshold:
+        return False
+    return True
 
 
 # Check if the averages of target attribute reverse if we de-aggregate data using test_attribute
-def check_for_avg_reverse(dataframe, agg_attribute, test_attribute, target_attribute):
+def check_for_bias(dataframe, agg_attribute, test_attribute, target_attribute):
     first_groups, first_att_vals = get_groups(dataframe, [agg_attribute])
     second_groups, second_att_vals  = get_groups(dataframe, [agg_attribute, test_attribute])
     first_avgs = get_groups_avgs(first_groups, target_attribute)
     second_avgs = get_groups_avgs(second_groups, target_attribute)
     first_group_avgs = {key:value for key, value in zip(first_att_vals, first_avgs)}
     second_group_avgs = {key:value for key, value in zip(second_att_vals, second_avgs)}
-    print(is_biased(first_group_avgs, second_group_avgs))
+    return is_biased(first_group_avgs, second_group_avgs)
 
+'''
+# Check if query is prone to the Simpson's paradox
+def is_prone_to_bias(query):
+    regex = r'^(SELECT)\s.*(AVG\s*\(.*\)).*\s(GROUP\s+BY)\s+[\w]+$'
+    if re.search(regex, query, re.IGNORECASE):
+        return True
+    return False '''
+
+
+def is_prone_to_bias(query):
+    select_seen, avg_seen, from_seen, group_by_seen = (False, False, False, False)
+    target_attributes = []
+    agg_attributes = []
+    tables = []
+    columns = []
+    parsed_query = sqlparse.parse(query)[0]
+    for token in parsed_query:
+        if select_seen:
+            if isinstance(token, sqlparse.sql.IdentifierList):
+                for identifier in token.get_identifiers():
+                    columns.append(identifier.value)
+                    print("{} {}\n".format("Attr = ", identifier) )
+            elif isinstance(token, sqlparse.sql.Identifier):
+                columns.append(token.value)
+                print("{} {}\n".format("Attr = ", token))
+            elif isinstance(token, sqlparse.sql.Function):
+                for sub_token in token.tokens:
+                    if avg_seen:
+                        for par_token in sub_token.tokens:
+                            if isinstance(par_token, sqlparse.sql.IdentifierList):
+                                for identifier in par_token.get_identifiers():
+                                    print("{} {}\n".format("Attr = ", identifier) )
+                                    target_attributes.append(identifier.value)
+                            elif isinstance(par_token, sqlparse.sql.Identifier):
+                                target_attributes.append(par_token.value)
+                                print("{} {}\n".format("Attr = ", par_token) )
+                        avg_seen = False
+                    if sub_token.value.upper() == "AVG":
+                        avg_seen = True
+        if from_seen:
+            if isinstance(token, sqlparse.sql.IdentifierList):
+                for identifier in token.get_identifiers():
+                    tables.append(identifier.value)
+                    print("{} {}\n".format("TAB = ", identifier) )
+            elif isinstance(token, sqlparse.sql.Identifier):
+                tables.append(token.value)
+                print("{} {}\n".format("TAB = ", token))
+        if group_by_seen:
+            if isinstance(token, sqlparse.sql.IdentifierList):
+                for identifier in token.get_identifiers():
+                    agg_attributes.append(identifier.value)
+                    print("{} {}\n".format("GROUPBY att = ", identifier) )
+            elif isinstance(token, sqlparse.sql.Identifier):
+                agg_attributes.append(token.value)
+                print("{} {}\n".format("GROUPBY att = ", token))
+            
+        if token.ttype is sqlparse.tokens.Keyword and token.value.upper() == "GROUP BY":
+            select_seen = False
+            from_seen = False
+            group_by_seen = True
+        if token.ttype is sqlparse.tokens.Keyword and token.value.upper() == "FROM":
+            select_seen = False
+            from_seen = True
+            group_by_seen = False
+        if token.ttype is sqlparse.tokens.DML and token.value.upper() == "SELECT":
+            select_seen = True
+            from_seen = False
+            group_by_seen = False
+        
+        if len(agg_attributes) != 0 and len(target_attributes) != 0:
+            return target_attributes, tables, agg_attributes, columns
+        return False
+
+
+# Check whether Simpson's paradox is present and correct it if present
+def detect_and_correct_execute(query):
+    pass
+
+# process query, detect and correct Simpson's paradox if present
+def process_query(query, sql_engine):
+    if is_prone_to_bias(query):
+        detect_and_correct_execute(query)
+    else:
+        sql_engine.execute(query)
 
 
 def main():
-    df = init_sql_db(dataset_dir)
-    check_for_avg_reverse(df, 'Treated', 'Gender', 'Survived')
+    print("1- Initialize SQL-database tables from CSV datasets")
+    option = input('Select option: ')
+    if option == '1':
+        init_sql_db(dataset_dir)
+    elif option == '2':
+        query = input('Query: ')
+        # query = 'SELECT avg(admit) from admissions_data GROUP BY Gender'
+        print(is_prone_to_bias(query))
+    # check_for_bias(df, 'Treated', 'Treated', 'Survived')
     # check_for_avg_reverse(df, 'Gender', 'Dept', 'Admit')
     # check_for_avg_reverse(df, 'player', 'year', 'outcome')
 
